e1297aadbb381741ee19a8aff6331ba9a8bd931f
pedroos <pedrosobota@gmail.com>
2018-01-27 21:11:06 -0200

    initial

---
+++ Lattice1/BigInt.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lattice1
+{
+    public static class BigInt
+    {
+        public static byte[] Factorial(byte[] n)
+        {
+            byte[] count = new byte[n.Length];
+            n.CopyTo(count, 0);
+            SubtractOneInPlace(count);
+
+            byte[] mult = new byte[n.Length];
+            n.CopyTo(mult, 0);
+            while (true)
+            {
+                mult = LatticeMultiplication(count, mult);
+                SubtractOneInPlace(count);
+                if (IsZero(count)) break;
+            }
+            return mult;
+        }
+
+        public static byte[] Factorial(uint n)
+        {
+            uint count = n - 1;
+
+            byte[] mult = MakeByteArray(n);
+            while (true)
+            {
+                byte[] countByte = MakeByteArray(count);
+                mult = LatticeMultiplication(countByte, mult);
+                count--;
+                if (count == 0) break;
+            }
+            return mult;
+        }
+
+        public static void SubtractOneInPlace(byte[] n)
+        {
+            if (n[n.Length - 1] >= 1)
+            {
+                n[n.Length - 1] = (byte)(n[n.Length - 1] - 1);
+                return;
+            }
+            n[n.Length - 1] = 9;
+            for (int i = n.Length - 1; i > 0; i--)
+            {
+                if (!(n[i - 1] > 0))
+                {
+                    n[i - 1] = 9;
+                    continue;
+                }
+                n[i - 1] = (byte)(n[i - 1] - 1);
+                break;
+            }
+        }
+
+        public static void MultiplyByLessOneInPlace(byte[] n)
+        {
+            byte[] result = new byte[n.Length * n.Length * 2];
+            for (int y = n.Length; y > 0; y--)
+            {
+                for (int x = n.Length; x > 0; x--)
+                {
+                    byte mx = n[x - 1];
+                    byte my;
+                    if (y == n.Length)
+                    {
+                        if (n[y - 1] >= 1)
+                        {
+                            my = (byte)(n[y - 1] - 1);
+                        }
+                        else
+                        {
+                            my = 9;
+                        }
+                    }
+                    else
+                    {
+                        if (n[y] >= 1)
+                        {
+                            my = n[y - 1];
+                        }
+                        else
+                        {
+                            if (n[y - 1] == 0)
+                            {
+                                my = 9;
+                            }
+                            else
+                            {
+                                my = (byte)(n[y - 1] - 1);
+                            }
+                        }
+                    }
+                    byte mult = (byte)(mx * my);
+
+                    int resultCell = ((y - 1) * n.Length * 2) + ((x - 1) * 2);
+                    if (mult < 10)
+                    {
+                        result[resultCell] = 0;
+                        result[resultCell + 1] = mult;
+                    }
+                    else
+                    {
+                        string _mult = mult.ToString();
+                        result[resultCell] = byte.Parse(_mult[0].ToString());
+                        result[resultCell + 1] = byte.Parse(_mult[1].ToString());
+                    }
+                }
+            }
+        }
+
+        private static bool IsZero(byte[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != 0) return false;
+            }
+            return true;
+        }
+
+        [Flags]
+        enum LatticeOrientation
+        {
+            None = 0,
+            Vertical = 1,
+            Horizontal = 2
+        }
+
+        public static byte[] LatticeMultiplication(byte[] a, byte[] b)
+        {
+            long d1 = a.Length;
+            long d2 = b.Length;
+            long x = d1;
+            long y = d2;
+            bool odd = false;
+            byte phase = 1;
+            LatticeOrientation orientation = LatticeOrientation.None;
+
+            byte[] digits = new byte[d1 + d2];
+            long digitPos = digits.Length;
+            long sum = 0;
+            byte acc = 0;
+
+            while (true)
+            {
+                // compute the multiplication
+                byte ma = a[x - 1];
+                byte mb = b[y - 1];
+                byte mult = (byte)(ma * mb);
+                string _mult = mult.ToString();
+                byte digit;
+                if (mult < 10)
+                {
+                    digit = odd ? (byte)0 : mult;
+                }
+                else
+                {
+                    digit = byte.Parse(_mult[odd ? 0 : 1].ToString());
+                }
+                sum += digit;
+
+                // check line ends
+                bool lineEndPhase1 = phase == 1 && y == d2 && !odd;
+                bool lineEndPhase2V = phase == 2 && orientation == LatticeOrientation.Vertical && x == 1 && odd;
+                bool lineEndPhase2H = phase == 2 && orientation == LatticeOrientation.Horizontal && y == d2 && !odd;
+                bool lineEndPhase3 = phase == 3 && x == 1 && odd;
+
+                if (lineEndPhase1 || lineEndPhase2V || lineEndPhase2H || lineEndPhase3)
+                {
+                    // finalize sum
+                    if (acc != 0)
+                    {
+                        sum += acc;
+                        acc = 0;
+                    }
+                    if (sum >= 10)
+                    {
+                        string _sum = sum.ToString();
+                        acc = byte.Parse(_sum[0].ToString());
+                        byte sumDigit = byte.Parse(_sum[1].ToString());
+                        digits[digitPos - 1] = sumDigit;
+                    }
+                    else
+                    {
+                        digits[digitPos - 1] = (byte)sum;
+                    }
+                    digitPos--;
+                    sum = 0;
+                }
+
+                if (lineEndPhase1)
+                {
+                    // check phase change
+                    if (x == d1 + 1 - d2)
+                    {
+                        orientation = orientation | LatticeOrientation.Horizontal;
+                    }
+                    if (x == 1)
+                    {
+                        orientation = orientation | LatticeOrientation.Vertical;
+                    }
+
+                    // phase changing
+                    if (orientation != LatticeOrientation.None)
+                    {
+                        if (orientation.HasFlag(LatticeOrientation.Horizontal) &&
+                            orientation.HasFlag(LatticeOrientation.Vertical))
+                        {
+                            phase = 3;
+                            // use the new phase jump, but:
+                            x = y;
+                            y = 1;
+                            odd = !odd; // and odd toggles to true
+                        }
+                        else
+                        {
+                            phase = 2;
+                            if (orientation == LatticeOrientation.Horizontal)
+                            {
+                                // use the new phase jump
+                                y = 1;
+                                x += d2 - 1;
+                                odd = !odd; // odd toggles to true
+                            }
+                            else if (orientation == LatticeOrientation.Vertical)
+                            {
+                                // don't use the new phase jump, use this phase's jump
+                                y -= d1 + 1 - x;
+                                x = d1;
+                                // odd stays false
+                            }
+                        }
+                    }
+                    else
+                    {
+                        y -= d1 + 1 - x;
+                        x = d1;
+                        // odd stays false
+                    }
+                }
+                else if (lineEndPhase2V)
+                {
+                    // check phase change
+                    if (y == d1 + 1)
+                    {
+                        phase = 3;
+                        // use the new phase jump
+                        x = y - 1;
+                        y = 1;
+                    }
+                    else
+                    {
+                        x = d1;
+                        y -= d1 + 1;
+                        odd = !odd; // odd toggles to false
+                    }
+                }
+                else if (lineEndPhase2H)
+                {
+                    // check phase change
+                    if (x == 1)
+                    {
+                        phase = 3;
+                        // don't use the new phase jump, use this phase's jump
+                        y = 1;
+                        x += d2 - 1;
+                        odd = !odd; // odd toggles to true
+                    }
+                    else
+                    {
+                        y = 1;
+                        x += d2 - 1;
+                        odd = !odd; // odd toggles to true
+                    }
+                }
+                else if (lineEndPhase3)
+                {
+                    // check end
+                    if (y == 1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        x = y - 1;
+                        y = 1;
+                        // odd stays true
+                    }
+                }
+                else
+                {
+                    // no end of line
+                    if (!odd)
+                    {
+                        y += 1;
+                    }
+                    else
+                    {
+                        x -= 1;
+                    }
+                    odd = !odd;
+                }
+            }
+
+            return digits;
+        }
+
+        public static byte[] MakeByteArray(ulong n)
+        {
+            if (n < 10) return new byte[] { (byte)n };
+            string _n = n.ToString();
+            byte[] nb = new byte[_n.Length];
+            for (int i = 0; i < _n.Length; i++)
+            {
+                nb[i] = byte.Parse(_n[i].ToString());
+            }
+            return nb;
+        }
+    }
+}
---
+++ Lattice1Tests/BigIntTests.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lattice1;
+using System.Linq;
+using System.Numerics;
+
+namespace Lattice1Tests
+{
+    [TestClass]
+    public class BigIntTests
+        {
+
+        [TestMethod]
+        public void SubtractOneInPlace1()
+        {
+            byte[] a = new byte[] { 1 };
+            BigInt.SubtractOneInPlace(a);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 0 }, a));
+        }
+
+        [TestMethod]
+        public void SubtractOneInPlace2()
+        {
+            byte[] a = new byte[] { 1, 2, 3, 4 };
+            BigInt.SubtractOneInPlace(a);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 1, 2, 3, 3 }, a));
+        }
+
+        [TestMethod]
+        public void SubtractOneInPlace3()
+        {
+            byte[] a = new byte[] { 1, 2, 3, 0 };
+            BigInt.SubtractOneInPlace(a);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 1, 2, 2, 9 }, a));
+        }
+
+        [TestMethod]
+        public void SubtractOneInPlace4()
+        {
+            byte[] a = new byte[] { 1, 8, 0, 0 };
+            BigInt.SubtractOneInPlace(a);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 1, 7, 9, 9 }, a));
+        }
+
+        [TestMethod]
+        public void SubtractOneInPlace5()
+        {
+            byte[] a = new byte[] { 0, 2, 0, 0 };
+            BigInt.SubtractOneInPlace(a);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 0, 1, 9, 9 }, a));
+        }
+
+        [TestMethod]
+        public void SubtractOneInPlace6()
+        {
+            byte[] a = new byte[] { 1, 0, 0 };
+            BigInt.SubtractOneInPlace(a);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 0, 9, 9 }, a));
+        }
+
+        [TestMethod]
+        public void SubtractOneInPlace7()
+        {
+            byte[] a = new byte[] { 1, 1, 0, 0 };
+            BigInt.SubtractOneInPlace(a);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 1, 0, 9, 9 }, a));
+        }
+
+        public static ulong PlainFactorial(ulong n)
+        {
+            ulong count = n - 1;
+
+            ulong mult = n;
+            while (true)
+            {
+                mult = count * mult;
+                count--;
+                if (count == 0) break;
+            }
+            return mult;
+        }
+
+        [TestMethod]
+        public void PlainFactorial19()
+        {
+            ulong result = PlainFactorial(19);
+            BigInteger r = NumericsFactorial(19);
+            Console.WriteLine("plain factorial result: " + result);
+            Assert.AreEqual(r.ToString(), result.ToString());
+        }
+
+        [TestMethod]
+        public void PlainFactorial29()
+        {
+            ulong result = PlainFactorial(29);
+            BigInteger r = NumericsFactorial(29);
+            Console.WriteLine("plain factorial result: " + result);
+            Assert.AreNotEqual(r.ToString(), result.ToString());
+        }
+
+        public static BigInteger NumericsFactorial(ulong n)
+        {
+            ulong count = n - 1;
+
+            BigInteger mult = new BigInteger(n);
+            while (true)
+            {
+                mult = BigInteger.Multiply(count, mult);
+                count--;
+                if (count == 0) break;
+            }
+            return mult;
+        }
+
+        public string ClearZeroes(byte[] result)
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] > 0)
+                {
+                    byte[] r = new byte[result.Length - i];
+                    Array.ConstrainedCopy(result, i, r, 0, result.Length - i);
+                    return string.Join("", r);
+                }
+            }
+            return string.Join("", result);
+        }
+
+        [TestMethod]
+        public void Factorial19()
+        {
+            TestFactorial(19);
+        }
+
+        [TestMethod]
+        public void Factorial29()
+        {
+            TestFactorial(29);
+        }
+
+        [TestMethod]
+        public void Factorial99()
+        {
+            TestFactorial(99);
+        }
+
+        [TestMethod]
+        void TestFactorial(ulong n)
+        {
+            byte[] nByte = BigInt.MakeByteArray(n);
+            byte[] result = BigInt.Factorial(nByte);
+            string r = ClearZeroes(result);
+            BigInteger result2 = NumericsFactorial(n);
+            Assert.AreEqual(r, result2.ToString());
+        }
+
+        [TestMethod]
+        public void Factorial199()
+        {
+            TestFactorial(199);
+        }
+
+        [TestMethod]
+        public void Factorial999()
+        {
+            TestFactorial(999);
+        }
+
+        [TestMethod]
+        public void Factorial1999()
+        {
+            TestFactorial(1999);
+        }
+
+        [TestMethod]
+        public void Factorial2999()
+        {
+            TestFactorial(2999);
+        }
+
+        [TestMethod]
+        public void Factorial4999()
+        {
+            TestFactorial(4999);
+        }
+
+        [TestMethod]
+        public void Factorial8999()
+        {
+            TestFactorial(8999);
+        }
+
+        [TestMethod]
+        public void LatticeMultiplication1()
+        {
+            byte[] a = new byte[] { 1, 2, 3, 4, 5 };
+            byte[] b = new byte[] { 1, 2, 3 };
+            byte[] result = BigInt.LatticeMultiplication(a, b);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 0, 1, 5, 1, 8, 4, 3, 5 }, result));
+        }
+
+        [TestMethod]
+        public void LatticeMultiplication2()
+        {
+            byte[] a = new byte[] { 1, 2, 3 };
+            byte[] b = new byte[] { 1, 2, 3, 4, 5 };
+            byte[] result = BigInt.LatticeMultiplication(a, b);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 0, 1, 5, 1, 8, 4, 3, 5 }, result));
+        }
+
+        [TestMethod]
+        public void LatticeMultiplication3()
+        {
+            byte[] a = new byte[] { 1, 2, 3, 4 };
+            byte[] b = new byte[] { 1, 2, 3, 4 };
+            byte[] result = BigInt.LatticeMultiplication(a, b);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 0, 1, 5, 2, 2, 7, 5, 6 }, result));
+        }
+
+        [TestMethod]
+        public void LatticeMultiplication4()
+        {
+            byte[] a = new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9 };
+            byte[] b = new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9 };
+            byte[] result = BigInt.LatticeMultiplication(a, b);
+            Assert.IsTrue(Enumerable.SequenceEqual(new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 8,
+            0, 0, 0, 0, 0, 0, 0, 0, 1}, result));
+        }
+    }
+}
